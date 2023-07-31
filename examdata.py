2b995a787f7793b9d6998a4836ae2e28fc6b70c2
andiesiwele <139359315+andiesiwele@users.noreply.github.com>
2023-07-31 13:11:50 +0200

    All functions working
    Currently doing functions for CS114 data

--- examdata.py
+++ examdata.py
@@ -1,10 +1,11 @@
 import numpy as np
-import scipy as sp
+#import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 import random
 
+'''
 # QUESTION 1 Create two datasets in csv files, one for each examination, with 150 rows each.
 def generate_age_group():
     age_groups = ['18 - 25', '25 - 35', '35 - 45', 'over 45']
@@ -46,70 +47,278 @@ with open('student_data_cs114.csv', mode='w', newline='') as file:
     writer.writerows(data_two)
 print("CSV file 'student_data_cs114.csv' with 150 rows has been created.")
 
+'''
 
 # QUESTION 2 Before performing operations on your data, you need to clean your data, remove any zero values and document the process thoroughly in your report.
 #create arrays from columns in student_data_math114
-student_num_math114 = np.array([])
-avg_hours_on_campus_math114 = np.array([])
-student_age_math114 = np.array([])
-student_mark_math114 = np.array([])
-#create arrays from columns in student_data_cs114
-student_num_cs114 = np.array([])
-avg_hours_on_campus_cs114 = np.array([])
-student_age_cs114 = np.array([])
-student_mark_cs114 = np.array([])
+# student_num_math114 = np.array([])
+# avg_hours_on_campus_math114 = np.array([])
+# student_age_math114 = np.array([])
+# student_mark_math114 = np.array([])
+# #create arrays from columns in student_data_cs114
+# student_num_cs114 = np.array([])
+# avg_hours_on_campus_cs114 = np.array([])
+# student_age_cs114 = np.array([])
+# student_mark_cs114 = np.array([])
+#
+# #fill arrays for math114 exam with loops taking info from csv files
+# with open('student_data_math114.csv', mode='r', newline='') as f:
+#     f.readline()
+#     for i in range(150):
+#         line = f.readline()
+#         x = line.split(',')
+#         np.append(student_num_math114, x[0])
+#         np.append(avg_hours_on_campus_math114, x[1])
+#         np.append(student_age_math114, x[2])
+#         np.append(student_mark_math114, x[3])
+# #fill arrays for cs114 exam with loops taking info from csv files
+# with open('student_data_math114.csv', mode='r', newline='') as f:
+#     f.readline()
+#     for i in range(150):
+#         line = f.readline()
+#         x = line.split(',')
+#         np.append(student_num_cs114, x[0])  #can i append numpy arrays like this???
+#         np.append(avg_hours_on_campus_cs114, x[1])
+#         np.append(student_age_cs114, x[2])
+#         np.append(student_mark_cs114, x[3])
+#
+# '''
+# #convert marks into percentages
+# student_mark_math114 = (student_mark_math114/130)*100
+# student_mark_cs114 = (student_mark_cs114/130)*100
 
-#fill arrays for math114 exam with loops taking info from csv files
-with open('student_data_math114.csv', mode='r', newline='') as f: 
-    f.readline()
-    for i in range(150): 
-        line = f.readline()
-        x = line.split(',')
-        np.append(student_num_math114, x[0])
-        np.append(avg_hours_on_campus_math114, x[1])
-        np.append(student_age_math114, x[2])
-        np.append(student_mark_math114, x[3])       
-#fill arrays for cs114 exam with loops taking info from csv files
+
+'''
+# 2.1 Create frequency tables for math114 and cs114 exams based on the following headings, and using the following criteria:
+
+# 2.1.1 a Create frequency table showing average hours Math114 students spent studying on campus
+# count number of students in each category of hours using the following counters
+one_two_hours_math114counter = 0 
+two_three_hours_math114counter = 0 
+four_five_hours_math114counter = 0 
 with open('student_data_math114.csv', mode='r', newline='') as f:
     f.readline()
-    for i in range(150): 
+    for i in range(150):
         line = f.readline()
-        x = line.split(',')
-        np.append(student_num_cs114, x[0])  #can i append numpy arrays like this???
-        np.append(avg_hours_on_campus_cs114, x[1])
-        np.append(student_age_cs114, x[2])
-        np.append(student_mark_cs114, x[3])
-    
-'''
-#convert marks into percentages
-student_mark_math114 = (student_mark_math114/130)*100 
-student_mark_cs114 = (student_mark_cs114/130)*100 
+        y = line.split(',')
+        if y[2] == '1 - 2':
+            one_two_hours_math114counter += 1  
+        elif y[2] == '2 - 3':
+            two_three_hours_math114counter += 1 
+        elif y[2] == '4 - 5':
+            four_five_hours_math114counter += 1
 
-'''
+data = {
+    'Average Hours Studying': ['1 - 2', '2 - 3', '4 - 5'],
+    'Frequency': [one_two_hours_math114counter, two_three_hours_math114counter, four_five_hours_math114counter]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing Average Hours Spent Studying on campus by Math114 Students"
+print(heading)
+print(frequency_table_df.to_string(index=False))
 
-'''
-# 2.1 Create frequency tables for math114 exam and cs114 exam based on the following headings, and using the following criteria:
-• Average hours spent on campus
-• Student age
-• Student mark
-https://www.statology.org/frequency-tables-python/
-'''
+# 2.1.1 b Create frequency table showing average hours CS114 students spent studying on campus
+# count number of students in each category of hours using the following counters
+one_two_hours_cs114counter = 0
+two_three_hours_cs114counter = 0
+four_five_hours_cs114counter = 0
+with open('student_data_cs114.csv', mode='r', newline='') as f:
+    f.readline()
+    for i in range(150):
+        line = f.readline()
+        y = line.split(',')
+        if y[2] == '1 - 2':
+            one_two_hours_cs114counter += 1
+        elif y[2] == '2 - 3':
+            two_three_hours_cs114counter += 1
+        elif y[2] == '4 - 5':
+            four_five_hours_cs114counter += 1
 
-'''
-'''
+data = {
+    'Average Hours Studying': ['1 - 2', '2 - 3', '4 - 5'],
+    'Frequency': [one_two_hours_cs114counter, two_three_hours_cs114counter, four_five_hours_cs114counter]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing Average Hours Spent Studying on campus by CS114 Students"
+print(heading)
+print(frequency_table_df.to_string(index=False))
+
+# 2.1.2 a) Create frequency table showing student ages of Math114 Students
+eighteen_twenty_five_age_math114counter = 0
+twenty_five_thirty_five_age_math114counter = 0
+thirty_five_forty_five_age_math114counter = 0
+over_forty_five_age_math114counter = 0
+with open('student_data_math114.csv', mode='r', newline='') as f:
+    f.readline()
+    for i in range(150):
+        line = f.readline()
+        y = line.split(',')
+        if y[1] == '18 - 25':
+            eighteen_twenty_five_age_math114counter += 1
+        elif y[1] == '25 - 35':
+            twenty_five_thirty_five_age_math114counter += 1
+        elif y[1] == '35 - 45':
+            thirty_five_forty_five_age_math114counter += 1
+        elif y[1] == 'over 45':
+            over_forty_five_age_math114counter += 1
+
+data = {
+    'Age Group': ['18 - 25', '25 - 35', '35 - 45', 'over 45'],
+    'Frequency': [eighteen_twenty_five_age_math114counter, twenty_five_thirty_five_age_math114counter, thirty_five_forty_five_age_math114counter, over_forty_five_age_math114counter]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing Number of Math114 Students in each Age Group "
+print(heading)
+print(frequency_table_df.to_string(index=False))
+
+# 2.1.2 b) Create frequency table showing student ages of Math114 Students
+eighteen_twenty_five_age_cs114counter = 0
+twenty_five_thirty_five_age_cs114counter = 0
+thirty_five_forty_five_age_cs114counter = 0
+over_forty_five_age_cs114counter = 0
+with open('student_data_cs114.csv', mode='r', newline='') as f:
+    f.readline()
+    for i in range(150):
+        line = f.readline()
+        y = line.split(',')
+        if y[1] == '18 - 25':
+            eighteen_twenty_five_age_cs114counter += 1
+        elif y[1] == '25 - 35':
+            twenty_five_thirty_five_age_cs114counter += 1
+        elif y[1] == '35 - 45':
+            thirty_five_forty_five_age_cs114counter += 1
+        elif y[1] == 'over 45':
+            over_forty_five_age_cs114counter += 1
+
+data = {
+    'Age Group': ['18 - 25', '25 - 35', '35 - 45', 'over 45'],
+    'Frequency': [eighteen_twenty_five_age_cs114counter, twenty_five_thirty_five_age_cs114counter, thirty_five_forty_five_age_cs114counter, over_forty_five_age_cs114counter]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing Number of CS114 Students in each Age Group "
+print(heading)
+print(frequency_table_df.to_string(index=False))
+
+
+# 2.1.3 a) Create frequency table showing Student marks of Math114 Students
+under_forty_five_math114 = 0
+forty_five_forty_nine_math114 = 0
+fifty_fifty_nine_math114 = 0
+sixty_sixty_nine_math114 = 0
+seventy_seventy_nine_math114 = 0
+over_eighty_math114 = 0
+
+with open('student_data_math114.csv', mode='r', newline='') as f:
+    f.readline()
+    for i in range(150):
+        line = f.readline()
+        y = line.split(',')
+        if int(y[3]) < 45:
+            under_forty_five_math114 += 1
+        elif 45 <= int(y[3]) < 49:
+            forty_five_forty_nine_math114 += 1
+        elif 50 <= int(y[3]) < 59:
+            fifty_fifty_nine_math114 += 1
+        elif 60 <= int(y[3]) < 69:
+            sixty_sixty_nine_math114 += 1
+        elif 70 <= int(y[3]) < 79:
+            seventy_seventy_nine_math114 += 1
+        elif int(y[3]) > 80:
+            over_eighty_math114 += 1
+
+data = {
+    "group_mark" : ['under 45','45 -49','50 - 59','60  - 69','70 - 79','over 80'],
+    "frequency" : [under_forty_five_math114, forty_five_forty_nine_math114, fifty_fifty_nine_math114, sixty_sixty_nine_math114, seventy_seventy_nine_math114, over_eighty_math114]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing marks of Math114 Students "
+print(heading)
+print(frequency_table_df.to_string(index=False))
+
+# 2.1.3 b) Create frequency table showing Student marks of CS114 Students
+under_forty_five_cs114 = 0
+forty_five_forty_nine_cs114 = 0
+fifty_fifty_nine_cs114 = 0
+sixty_sixty_nine_cs114 = 0
+seventy_seventy_nine_cs114 = 0
+over_eighty_cs114 = 0
+
+with open('student_data_cs114.csv', mode='r', newline='') as f:
+    f.readline()
+    for i in range(150):
+        line = f.readline()
+        y = line.split(',')
+        if int(y[3]) < 45:
+            under_forty_five_cs114 += 1
+        elif 45 <= int(y[3]) < 49:
+            forty_five_forty_nine_cs114 += 1
+        elif 50 <= int(y[3]) < 59:
+            fifty_fifty_nine_cs114 += 1
+        elif 60 <= int(y[3]) < 69:
+            sixty_sixty_nine_cs114 += 1
+        elif 70 <= int(y[3]) < 79:
+            seventy_seventy_nine_cs114 += 1
+        elif int(y[3]) > 80:
+            over_eighty_cs114 += 1
+
+data = {
+    "group_mark" : ['under 45','45 -49','50 - 59','60  - 69','70 - 79','over 80'],
+    "frequency" : [under_forty_five_cs114, forty_five_forty_nine_cs114, fifty_fifty_nine_cs114, sixty_sixty_nine_cs114, seventy_seventy_nine_cs114, over_eighty_cs114]
+}
+# Creating a DataFrame from the frequency table data
+frequency_table_df = pd.DataFrame(data)
+heading = "Frequency Table showing marks of CS114 Students "
+print(heading)
+print(frequency_table_df.to_string(index=False))
+''''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # 2.2 Using Matplotlib, complete the following:
+# 2.2.1 a) Use a bar chart to show the age groups (x) and numbers of Math114 students
 age_groups = ['18 - 25', '25 - 35', '35 - 45', 'over 45']
-
-#Use a bar chart to show the ages and numbers of students. Your x-axis should represent the age groups used in the previous question.
-age_groups = ['18 - 25', '25 - 35', '35 - 45', 'over 45']
-student_age_num = np.array() #count how many student in each age range and put in array
-
-#count numebr of students in each age group
-#counters for age ranges
+# count how many student in each age range and put in array
+student_age_num = np.array([])
+# count number of students in each age group by creating counters for each age group
 eighteen_twenty_five = 0
 twenty_five_thirty_five = 0
-thirt_five_forty_five = 0
+thirty_five_forty_five = 0
 over_forty_five = 0
+eighteen_twenty_five_age_math114counter = 0
+twenty_five_thirty_five_age_math114counter = 0
+thirty_five_forty_five_age_math114counter = 0
+over_forty_five_age_math114counter = 0
 with open('student_data_math114.csv', mode='r', newline='') as f:
     f.readline()
     for i in range(150):
@@ -120,57 +329,145 @@ with open('student_data_math114.csv', mode='r', newline='') as f:
         elif line_info[1] == '25 - 35':
             twenty_five_thirty_five += 1
         elif line_info[1] == '35 - 45':
-            thirt_five_forty_five += 1
+            thirty_five_forty_five += 1
         elif line_info[1] == 'over 45':
             over_forty_five += 1
-######## REPEAT FOR CS114
 
-student_age_num = np.append(eighteen_twenty_five)
-student_age_num = np.append(twenty_five_thirty_five)
-student_age_num = np.append(thirt_five_forty_five)
-student_age_num = np.append(over_forty_five)
+student_age_num = np.append(student_age_num, eighteen_twenty_five)
+student_age_num = np.append(student_age_num, twenty_five_thirty_five)
+student_age_num = np.append(student_age_num, thirty_five_forty_five)
+student_age_num = np.append(student_age_num, over_forty_five)
 
-plt.bar(age_groups,student_age_num)
+# Putting arrays into numpy array functions, so we can use in matplotlib
+x = np.array(age_groups)
+y = student_age_num
+
+plt.bar(x, y)
 plt.show()
+labelling x-axis and y-axis
+plt.xlabel('Age Group')
+plt.ylabel('Number of Students')
+displaying the title
+plt.title("Bar Graph showing number of Students per age group")
+
+# REPEAT FOR CS114 change counter names to indicate exam sub
+# 2.2.1 b) Use a bar chart to show the age groups (x) and numbers of CS114 students
 
 
-#Use a line graph to show if there is a correlation between higher marks(y) and more time spent on campus(x).
-x = np.array([])
-y = np.array([])
-with open('student_data_math114.csv', mode='r', newline='') as f:
-    f.readline()
-    for i in range(150):
-        line = f.readline()
-        line_info = line.split(',')
-        np.append(x, line_info[3])
-        np.append(y, line_info[2])
-plt.plot(x, y)
-plt.show()
 
-#Use a scatter chart to plot each student’s mark(y) and the time(x) taken on the examination, in minutes.
-x = np.array([])
-y = np.array([])
-with open('student_data_math114.csv', mode='r', newline='') as f:
-    f.readline()
-    for i in range(150):
-        line = f.readline()
-        line_info = line.split(',')
-        np.append(y, line_info[3])
-        np.append(x, line_info[4])
-plt.scatter(x, y)
-plt.show()
+#2.2.2 a) Use a line graph to show if there is a correlation between higher Math114 marks(y) and more time spent on campus(x).
 
-# Use a scatter chart to plot the relationship between time spent on campus(y) and the student’s age(x).
-x = np.array([])
-y = np.array([])
-with open('student_data_math114.csv', mode='r', newline='') as f:
-    f.readline()
-    for i in range(150):
-        line = f.readline()
-        line_info = line.split(',')
-        np.append(y, line_info[1])
-        np.append(x, line_info[2])
-plt.scatter(x, y)
-plt.show()
+# variables to store num of student in each time category
+# num_students_one_two = 0
+# num_students_two_three = 0
+# num_students_four_five = 0
+# study_hours = ['1 - 2', '2 - 3', '4 - 5']
+# x = np.array(study_hours)
+# sum_grades_one_two = 0
+# sum_grades_two_three = 0
+# sum_grades_four_five = 0
+#
+# y = np.array([])
+# # get average study hours for each group
+# with open('student_data_math114.csv', mode='r', newline='') as f:
+#     f.readline()
+#     for i in range(150):
+#         line = f.readline()
+#         line_info = line.split(',')
+#         if line_info[2] == '1 - 2':
+#             #get percentage of each student's mark, then add it to grades_students_one_two and
+#             sum_grades_one_two = sum_grades_one_two + (int(line_info[3])/130) *100
+#             num_students_one_two += 1
+#         elif line_info[2] == '2 - 3':
+#             sum_grades_two_three = sum_grades_two_three + (int(line_info[3])/130) *100
+#             num_students_two_three += 1
+#         elif line_info[2] == '4 - 5':
+#             sum_grades_four_five = sum_grades_four_five + (int(line_info[3])/130) *100
+#             num_students_four_five += 1
+#
+# avg_one_two = sum_grades_one_two/num_students_one_two
+# avg_two_three = sum_grades_two_three/num_students_two_three
+# avg_four_five = sum_grades_four_five/num_students_four_five
+# average = np.array([avg_one_two, avg_two_three, avg_four_five])
+# y = np.append(y, num_students_one_two)
+# y = np.append(y, num_students_two_three)
+# y = np.append(y, num_students_four_five)
+#
+# plt.plot(x, y)
+# # labelling x-axis and y-axis
+# plt.xlabel('time spent on campus')
+# plt.ylabel('Student Mark')
+# # displaying the title
+# plt.title('line graph showing correlation between marks and time spent on campus')
+# plt.show()
+
+#2.2.2 b) Use a line graph to show if there is a correlation between higher CS114 marks(y) and more time spent on campus(x).
 
 
+
+
+#2.2.3 a) Use a scatter chart to plot each student’s mark(y) and the time(x) taken on the Math114 examination, in minutes.
+# def sort_array(array):
+#     # bubble sort
+#     for first_idx, first_item in enumerate(array):
+#         for second_idx, second_item in enumerate(array):
+#             if int(array[first_idx][4]) < int(array[second_idx][4]):
+#                 # TODO: make sort make sense
+#                 smaller = array[second_idx]
+#                 bigger = array[first_idx]
+#                 array[first_idx] = smaller
+#                 array[second_idx] = bigger
+#     return array
+#
+# x = np.array([]) #time taken
+# y = np.array([]) #student mark
+#
+# new_array = []
+# with open('student_data_math114.csv', mode='r', newline='') as file:
+#     file.readline()
+#     for line in file:
+#         stripped_line = line.rstrip('\r\n')
+#         line_items = stripped_line.split(',')
+#         new_array.append(line_items)
+#     # sort array of lines in ascending order by time which is index 4 of each line
+#
+# sorted_array = sort_array(new_array)
+# for sorted_item in sorted_array:
+#     # [student_number, age_group, hours_studying, student_mark, time_taken_on_exam]
+#     y = np.append(y, (int(sorted_item[3])/130) *100)
+#     x = np.append(x, int(sorted_item[4]))
+# plt.scatter(x, y)
+#
+# # labelling x-axis and y-axis
+# plt.xlabel('Time Spent On Exam')
+# plt.ylabel('Student Mark')
+# # # displaying the title
+# plt.title('Scatter Chart of Student Marks and Time spent on Math114 Exam')
+# plt.show()
+
+
+#2.2.3 b) Use a scatter chart to plot each student’s mark(y) and the time(x) taken on the CS114 examination, in minutes.
+
+# TODO: Complete the below!!!!!!!!!!!!!!!!!!
+# #2.2.4 a)  Use a scatter chart to plot the relationship between time spent on campus(y)and age of Math114 students.
+# x = np.array([])
+# y = np.array([])
+# with open('student_data_math114.csv', mode='r', newline='') as f:
+#     f.readline()
+#     for i in range(150):
+#         line = f.readline()
+#         line_info = line.split(',')
+#         np.append(y, line_info[1])
+#         np.append(x, line_info[2])
+# plt.scatter(x, y)
+
+# labelling x-axis and y-axis
+# plt.xlabel('time spent on campus')
+# plt.ylabel('marks')
+# # displaying the title
+# plt.title('line graph showing correlation between time spent on campus and age of Math114 students')
+
+# plt.show()
+#
+
+# #2.2.4 b)  Use a scatter chart to plot the relationship between time spent on campus(y)and age of CS114 students.
